e7b4778b4de0860f0a7068bf856090f6750dc55b
nir koren <nirkoren1@gmail.com>
2021-11-07 21:31:30 +0200

    .

--- ex_1.c
+++ ex_1.c
@@ -1,10 +1,31 @@
+/***********************
+* Nir koren
+* 316443902
+* ass01
+***********************/
+
+
 #include <stdio.h>
 #include <math.h>
 
+
+/******************
+* Function Name: hello
+* Input: None
+* Output: printing "hello \/\/orld %d\n"
+* Function Operation: using the printf function and double // %% \\
+******************/
 void hello(){
     printf("hello \\/\\/orld %%d\\n\n");
 }
 
+
+/******************
+* Function Name: distance
+* Input: two points (x1, y1), (x2, y2)
+* Output: the distance between the points
+* Function Operation: using the distance formula (pitagorian triangle) sqrt((x1 - x2)^2 + (y1 - y2)^2)
+******************/
 void distance(){
     double x1, x2, y1, y2, dist;
     printf("Enter x1:\n");
@@ -19,6 +40,13 @@ void distance(){
     printf("The distance between the points is: %lf\n", dist);
 }
 
+
+/******************
+* Function Name: conversions
+* Input: distance in nano meters
+* Output: the same distance in  mm, cm, dm, m, km and nn with 5 numbers before the decimal point and 4 after
+* Function Operation: using the formulas mm = nm / 1e6, cm = mm / 10, dm = cm / 10, m = dm / 10, km = m / 1000
+******************/
 void conversions(){
     int nm;
     double km, m, dm, cm, mm;
@@ -37,6 +65,13 @@ void conversions(){
     printf("%010d nm\n", nm);
 }
 
+
+/******************
+* Function Name: baking
+* Input: baking time of the cake, the time it was put in the oven
+* Output: the time when you should get it out of the oven
+* Function Operation: adding the time together using modulo and with the remainder
+******************/
 void baking(){
     int add_h, add_m, add_s, start_h, start_m, start_s, final_h, final_m, final_s;
     printf("Please enter the baking time: [hh:mm:ss]\n");
