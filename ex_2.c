4f0c24d0a0f3a59d7ea2b69ae8e9066bead25df0
nir koren <nirkoren1@gmail.com>
2021-11-11 12:03:40 +0200

    .

---
+++ ex_2.c
@@ -0,0 +1,24 @@
+/***********************
+* Nir koren
+* 316443902
+* ass02
+***********************/
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+int main(){
+    printf("Choose an option:\n"
+           "        1. hexadecimal to Decimal\n"
+           "        2. Decimal to Base\n"
+           "        3. Base to Decimal\n"
+           "        4. PLUS\n"
+           "        5. Shape\n"
+           "        6. Count bits\n"
+           "        7. Exit\n");
+    return 0;
+}
