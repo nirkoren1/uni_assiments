6353ad09a670eb5aa82c72b3cbd3f8900ebcf24c
nir koren <nirkoren1@gmail.com>
2021-11-13 13:11:58 +0200

    .

--- ex_2.c
+++ ex_2.c
@@ -8,17 +8,63 @@
 #include <stdio.h>
 #include <math.h>
 
+int init(){
+    int key;
+    while (1){
+        printf("Choose an option:\n"
+               "        1. hexadecimal to Decimal\n"
+               "        2. Decimal to Base\n"
+               "        3. Base to Decimal\n"
+               "        4. PLUS\n"
+               "        5. Shape\n"
+               "        6. Count bits\n"
+               "        7. Exit\n");
+        scanf("%d", &key);
+        if (key <= 7)
+            return key;
+        printf("Wrong option!\n");
+
+    }
+}
 
 
+void hextodec(){
+    char hexnum;
+    printf("Enter a reversed number in base 16:\n");
+    while (1){
+        scanf("%c", &hexnum);
+        if (hexnum == 'e') {
+            printf("%c", hexnum);
+            return;
+        }
+    }
+
+}
 
 int main(){
-    printf("Choose an option:\n"
-           "        1. hexadecimal to Decimal\n"
-           "        2. Decimal to Base\n"
-           "        3. Base to Decimal\n"
-           "        4. PLUS\n"
-           "        5. Shape\n"
-           "        6. Count bits\n"
-           "        7. Exit\n");
-    return 0;
+    int key;
+
+    key = init();
+    switch (key) {
+        case 1:
+            hextodec();
+            break;
+//        case 2:
+//            dectobase();
+//            break;
+//        case 3:
+//            basetodec();
+//            break;
+//        case 4:
+//            plus();
+//            break;
+//        case 5:
+//            shape();
+//            break;
+//        case 6:
+//            cntbits();
+//            break;
+        default:
+            return 0;
+    }
 }
