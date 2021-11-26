c4b15ea8837e16b698f563045223bd3a45d10b6d
Nir <nirkoren1@gmail.com>
2021-11-27 01:41:25 +0200

    .

--- ex_3.c
+++ ex_3.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <math.h>
 #define SIZE 100
+#define LIMIT 30
+
 
 
 int is_prime(int num){
@@ -52,24 +54,54 @@ void gold(){
 int inverse_num(int num){
     int output = 0;
     while (1){
-
+        output += (num % 10) ;
+        num /= 10;
+        if (num == 0)
+            return output;
+        output *= 10;
     }
-
 }
 
 
 int is_poly(int num){
-
+    if (num == inverse_num(num))
+        return 1;
+    return 0;
 }
 
 
 void lycherl(){
+    for (int i = 1; i < LIMIT ; i++) {
+        printf("%d", i);
+        int result = i, rev;
+        while (!is_poly(result)){
+            rev = inverse_num(result);
+            printf(":%d+%d=%d", result, rev, result + rev);
+            result += rev;
+        }
+        printf("\n");
+    }
+}
+
+
+int get_longest_ser(int lst[], int i, int lst_len){
+    int result = 0;
 
 }
 
 
 void longest(){
-
+    int lst_len, max, tmp_longest;
+    printf("Enter the number of elements\n");
+    scanf("%d", &lst_len);
+    int lst[lst_len];
+    for (int i = 0; i < lst_len; i++) {
+        scanf("%d", &lst_len[i]);
+    }
+    for (int i = 0; i < lst_len; i++) {
+        tmp_longest = get_longest_ser(lst, i, lst_len);
+        max = max > tmp_longest ? max : tmp_longest;
+    }
 }
 
 
@@ -89,10 +121,10 @@ int main(){
 //            gold();
 //            break;
 //        case 3:
-            lycherl();
+//            lycherl();
 //            break;
 //        case 4:
-//            longest;
+            longest;
 //            break;
 //        default:
 //            printf("Wrong option!");
