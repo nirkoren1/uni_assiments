e1ae9d9479e635996a4e287a4ee6b1690861eddc
nir koren <nirkoren1@gmail.com>
2021-12-05 23:26:45 +0200

    .

--- ex_4.c
+++ ex_4.c
@@ -9,33 +9,40 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#define WORDSSIZE 15
+#define SIZEPERWORD 20
 
 
-char *string_to_tokens_lst(char str[], char delim[]){
+char **string_to_tokens_lst(char str[], char delim[], int *size){
     int i = -1;
-    char out[15][20];
     char *token = strtok(str, delim);
+    char **out = (char **) malloc(sizeof (char *));
+    token = strtok(NULL, delim);
     while (token != NULL) {
         i += 1;
+        *size += 1;
+        out = (char **) realloc(out, sizeof (char *) * (i + 1));
+        out[i] = (char *) malloc(sizeof(char) * strlen(token) + 1);
         strcpy(out[i], token);
         token = strtok(NULL, delim);
     }
-    for (int j = 0; j < i + 1; j++) {
-        printf("%s\n", out[j]);
-    }
+    return out;
 }
 
 char *init(){
-    int size_names = 15, size_per_name = 20;
-    char names[15 * 20];
+    char words[WORDSSIZE * SIZEPERWORD] = {"\0"}, **token_ptr;
+    int size = 0;
     printf("Enter your words:\n");
-    scanf("%s", names);
-    string_to_tokens_lst("name:Avi cohen,Moshe,Dana!", ":,");
+    fgets(words, WORDSSIZE * SIZEPERWORD, stdin);
+    token_ptr = string_to_tokens_lst(words, ":,", &size);
+    lexicografic_sort(token_ptr);
+    for (int j = 0; j < size; j++) {
+        printf("%s\n", token_ptr[j]);
+    }
 }
 
 
 int main(){
-//    init();
-    string_to_tokens_lst("name:Avi cohen,Moshe,Dana!", ":,");
+    init();
 //    hang_man();
 }
