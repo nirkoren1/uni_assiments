e485ba1191700880c9ebb5c4c2cf4505c5ab45d9
nir koren <nirkoren1@gmail.com>
2022-01-05 20:02:04 +0200

    .

--- person.c
+++ person.c
@@ -1,8 +1,3 @@
-//
-// Created by Nirkoren on 05/01/2022.
-//
-
-
 
 #include <string.h>
 
@@ -20,7 +15,19 @@ int compare_str(void *p1, void *p2) {
 }
 
 
-int compare_bmi(void *y1, void *m1, void *d1, void *y2, void *m2, void *d2){
-
+int compare_age(void *y1, void *m1, void *d1, void *y2, void *m2, void *d2){
+    int val = compare_int(y1, y2);
+    if (val != 0)
+        return val;
+    val = compare_int(m1, m2);
+    if (val != 0)
+        return val;
+    val = compare_int(d1, d2);
+    if (val != 0)
+        return val;
+    return val;
 }
 
+int compare_bmi(void *p_h1, void *p_w1, void *p_h2, void *p_w2){
+
+}
