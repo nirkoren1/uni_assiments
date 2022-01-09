c58606bbeb157b99afaafe44c7c6f9c1cb3ce2fc
Nir <nirkoren1@gmail.com>
2022-01-09 02:35:54 +0200

    .

--- ex_6.c
+++ ex_6.c
@@ -1,15 +1,15 @@
 #include "sorting.h"
 #include "person.h"
 #define MAX_LINES 100
-const char* INPUTFILE = "C:\\Users\\Nirkoren\\CLionProjects\\uni_assiments\\persons.txt";
-const char* OUTPUTFILE = "C:\\Users\\Nirkoren\\CLionProjects\\uni_assiments\\sorted.txt";
+const char* INPUTFILE = "persons.txt";
+const char* OUTPUTFILE = "sorted.txt";
 
 
 int main() {
     Person persons[MAX_LINES];
     int numOfPersons = 0;
     load(INPUTFILE, persons, &numOfPersons);
-    quicksort(persons, 0, numOfPersons, swapPersons, comparePersonByFirstName);
+    quicksort(persons, 0, numOfPersons, swapPersons, comparePersonByWeight);
     save(OUTPUTFILE, persons, numOfPersons);
     return 0;
 }
