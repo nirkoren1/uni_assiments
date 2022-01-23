37adaa6346d87df97026f82fb3a0254b54420269
Nir <nirkoren1@gmail.com>
2022-01-23 16:35:53 +0200

    .

---
+++ practice.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int GCDRec(int large, int small, int iter){
+    if (num2 == 1 || num1 == 1)
+        return 1;
+    GCD(nu)
+}
+
+int GCD(int num1, int num2){
+    return num1 > num2 ? GCDRec(num1, num2, num2) : GCDRec(num2, num1, num1);
+}
+
+int main()
+{
+    printf("%d", GCD(50, 10));
+}
+
